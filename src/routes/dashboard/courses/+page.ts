98090eecafda851c47e651be2551cfd3fc178c4f
Ethan <ethanmccaskey2@gmail.com>
2023-02-10 20:10:53 -0700

    Learning and working about image caching ugh

--- src/routes/dashboard/courses/+page.ts
+++ src/routes/dashboard/courses/+page.ts
@@ -9,8 +9,27 @@ export const load: PageLoad = async (event) => {
     const { data: courseData, error } = await supabaseClient.from('courses')
       .select('id, course_image_url, course_title, course_prefix, course_number, course_term')
       .eq('created_by', session.user.id);
+
     return {
       courseData
     };
   }
 };
+
+// This code ended up being not needed but will be left here, because I forget how I did this.
+async function getCourseImages(image: URL | RequestInfo){
+  // Convert url from database into blob
+  return await fetch(image,
+    {
+      method: 'GET',
+      headers: {
+        credentials: 'omit',
+        'cache-control': 'max-age=3600'
+      }
+    })
+    .then((response) => response.blob())
+    .then((myBlob) =>
+    {
+      return URL.createObjectURL(myBlob);
+    })
+}
