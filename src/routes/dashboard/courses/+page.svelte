71cc82c4dac7ba8562903ae14ddca5ef5ddb1c26
ethan-mcc <ethanmccaskey2@gmail.com>
2023-02-17 17:21:26 -0700

    for desktop

--- src/routes/dashboard/courses/+page.svelte
+++ src/routes/dashboard/courses/+page.svelte
@@ -49,8 +49,22 @@
         </div>
       </div>
     </section>
+    <section class="fixed inset-y-0 z-10 flex-shrink-0 w-64 bg-gray-600 border-r dark:border-primary-darker dark:bg-darker lg:static focus:outline-none">
+      <div class="flex flex-col h-screen">
+        <!-- Panel header -->
+        <div class="flex-shrink-0">
+          <div class="px-4 pt-4 border-b dark:border-primary-darker">
+            <h2 id="secondSidebarHeader" class="pb-4 font-semibold">Header</h2>
+          </div>
+        </div>
 
-    <section class="p-8">
+        <!-- Panel content -->
+
+      </div>
+    </section>
+
+
+    <section class="flex flex-col p-8">
 
       <a href="/dashboard/courses/create">
         <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-300 to-blue-500 group-hover:from-blue-300 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800">
