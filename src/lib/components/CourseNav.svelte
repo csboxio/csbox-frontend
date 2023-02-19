74750bfa228363b5697de53582e93c00b7ee4747
ethan-mcc <ethanmccaskey2@gmail.com>
2023-02-19 08:58:02 -0800

    removed grouping and switched to just the slug route

--- src/lib/components/CourseNav.svelte
+++ src/lib/components/CourseNav.svelte
@@ -1,5 +1,9 @@
 <script>
   import {page} from "$app/stores";
+
+  export let data;
+  console.log($page.params.slug)
+
 </script>
 
 <section class="fixed inset-y-0 z-10 flex-shrink-0 w-60 bg-gray-600 border-r dark:border-primary-darker dark:bg-darker lg:static focus:outline-none">
@@ -7,12 +11,12 @@
     <!-- Panel content -->
     <div class="flex-1 p-3 overflow-y-hidden text-lg font-semibold text-white ">
       <!-- Content -->
-      <a href= {$page.params.slug}>
+      <a href= {data.slug}>
         <div class="space-y-8 py-3 hover:bg-gray-800 rounded-xl">
           <p class="px-4">Home</p>
         </div>
       </a>
-      <a href= {$page.params.slug + "/assignments"}>
+      <a href= {"/assignments"}>
         <div class="space-y-8 py-3 hover:bg-gray-800 rounded-xl">
           <p class="px-4">Assignments</p>
         </div>
