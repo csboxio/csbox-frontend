3e83e46e6e8a5497c6a24e82230fcebf89438fe4
ethan-mcc <ethanmccaskey2@gmail.com>
2023-02-22 15:07:58 -0700

    Working on blur for background when show create box on layout

--- src/routes/dashboard/courses/[slug]/+layout.svelte
+++ src/routes/dashboard/courses/[slug]/+layout.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <body class="bg-gray-600 antialiased bg-body text-body font-body" on:click|stopPropagation={() => model.handleToggleMenuTopRight("outside")} >
-<div class="">
+<div class="{show_create_box ? 'filter blur-[1px]' : ''}">
   <Navbar/>
   <div class="mx-auto lg:ml-20">
     <section>
