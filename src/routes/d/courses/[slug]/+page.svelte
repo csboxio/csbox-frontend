4c25addce9153e8336db6d5285b1610c896ea263
ethan-mcc <ethanmccaskey2@gmail.com>
2023-03-28 11:26:44 -0700

    UI fixes

--- src/routes/d/courses/[slug]/+page.svelte
+++ src/routes/d/courses/[slug]/+page.svelte
@@ -86,22 +86,19 @@
 <svelte:options accessors={true}/>
 <div class="flex flex-row grow max-w-full-1/2">
     <section class="p-1 grow max-w-full-1/2 ">
-      <div class="flex flex-row grow max-w-full-1/2">
-        <h4 class="flex flex-row text-xl font-bold text-white -mx-auto my-5">Home</h4>
-
-
-      {#if mode.view}
-
-        <button class="flex flex-row float-right justify-center p-0.5 mb-6 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-300 group-hover:from-blue-300 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800"
-                on:click={handleEdit}>
+        <div class="flex justify-between">
+        <h4 class="text-xl font-bold text-white -mx-auto my-5">Home</h4>
+                {#if mode.view}
+          <div class="">
+              <button class="my-4 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-300 group-hover:from-blue-300 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800"
+                  on:click={handleEdit}>
                 <span class="relative px-5 py-2.5 transition-all|local ease-in duration-75 bg-white dark:bg-gray-600 rounded-md group-hover:bg-opacity-0">
                   Edit
               </span>
         </button>
-
+          </div>
       {/if}
-      </div>
-
+        </div>
         {#if mode.edit}
         <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-300 group-hover:from-blue-300 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800"
                 on:click={handleSave}>
