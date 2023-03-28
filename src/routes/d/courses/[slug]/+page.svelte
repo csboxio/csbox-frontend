b9fa1f6499f370141da673a8861050893dea4b4d
Ethan <ethanmccaskey2@gmail.com>
2023-03-27 19:40:30 -0700

    t

--- src/routes/d/courses/[slug]/+page.svelte
+++ src/routes/d/courses/[slug]/+page.svelte
@@ -84,18 +84,23 @@
 
 </script>
 <svelte:options accessors={true}/>
-<div class="flex flex-row grow max-w-full">
-    <section class="p-1 grow max-w-full">
-        <h4 class="text-xl font-bold text-white -mx-auto my-5">Home</h4>
+<div class="flex flex-row grow max-w-full-1/2">
+    <section class="p-1 grow max-w-full-1/2 ">
+      <div class="flex flex-row grow max-w-full-1/2">
+        <h4 class="flex flex-row text-xl font-bold text-white -mx-auto my-5">Home</h4>
 
-        {#if mode.view}
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-300 group-hover:from-blue-300 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800"
-        on:click={handleEdit}>
+
+      {#if mode.view}
+
+        <button class="flex flex-row float-right justify-center p-0.5 mb-6 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-300 group-hover:from-blue-300 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800"
+                on:click={handleEdit}>
                 <span class="relative px-5 py-2.5 transition-all|local ease-in duration-75 bg-white dark:bg-gray-600 rounded-md group-hover:bg-opacity-0">
                   Edit
               </span>
         </button>
-        {/if}
+
+      {/if}
+      </div>
 
         {#if mode.edit}
         <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-blue-300 group-hover:from-blue-300 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800"
@@ -110,23 +115,29 @@
                   Preview
               </span>
             </button>
-
-            <button class="relative inline-flex float-right mr-4 items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-500 to-red-300 group-hover:from-blue-300 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800"
-                    on:click={handleCancel}>
+          <button class="relative inline-flex float-right items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-500 to-red-300 group-hover:from-blue-300 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-red-200 dark:focus:ring-red-800"
+                  on:click={handleCancel}>
                 <span class="relative px-5 py-2.5 transition-all|local ease-in duration-75 bg-white dark:bg-gray-600 rounded-md group-hover:bg-opacity-0">
                   Cancel
               </span>
-            </button>
+          </button>
         {/if}
-        <div class="container my-6 rounded-lg">
+
+        <div class=" my-6 rounded-lg ">
+
+
             <div transition:blur|local="{{duration: 200}}" class="flex flex space-x-4 grow mr-4 rounded-lg">
-                    <div transition:blur|local="{{duration: 200}}" class="flex-1 w-1/2 bg-white min-h-screen border-0 rounded-lg" hidden="{mode.edit === true ? '' : 'hidden'}" >
-                        <div transition:blur|local="{{duration: 200}}" id="editor" class="editor bg-white" hidden="{mode.edit === true ? '' : 'hidden'}"></div>
+                    <div transition:blur|local="{{duration: 200}}" class="flex-1 w-1/2 bg-white min-h-screen border-0 rounded-lg"
+                         hidden="{mode.edit === true ? '' : 'hidden'}">
+                        <div transition:blur|local="{{duration: 200}}" id="editor" class="editor bg-white"
+                             hidden="{mode.edit === true ? '' : 'hidden'}"></div>
                     </div>
-                  <div class="editor flex-1 w-1/2 bg-white p-2 min-h-screen border-0 rounded-lg" hidden="{mode.edit === false ? '' : 'hidden'}">
+                  <div class="editor flex-1 w-1/2 bg-white p-2 min-h-screen border-0 rounded-lg"
+                       hidden="{mode.edit === false ? '' : 'hidden'}">
                     {@html content.html}
                   </div>
             </div>
+
         </div>
     </section>
 </div>
