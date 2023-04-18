e7ae4a9915bd33413a5737ba27df38d18fc67f26
Ethan <ethanmccaskey2@gmail.com>
2023-04-17 17:46:46 -0700

    depen issues

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -41,6 +41,20 @@
             {/if}
 
           </li>
+
+          <li>
+            {#if $page.route.id === '/d/instances'}
+              <a class="flex items-center justify-center w-12 h-12 rounded-xl text-blue-500  hover:bg-gray-800 mb-2" sveltekit:prefetch href='/d/instances'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" aria-label="server" viewBox="0 0 512 512"><path d="M64 32C28.7 32 0 60.7 0 96v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM344 152c-13.3 0-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24zm96-24c0 13.3-10.7 24-24 24s-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24zM64 288c-35.3 0-64 28.7-64 64v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V352c0-35.3-28.7-64-64-64H64zM344 408c-13.3 0-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24zm104-24c0 13.3-10.7 24-24 24s-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24z"></path></svg>
+              </a>
+            {:else }
+              <a class="flex items-center justify-center w-12 h-12 rounded-xl text-gray-400 hover:text-blue-500 hover:bg-gray-800 mb-2" sveltekit:prefetch href='/d/instances'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" aria-label="server" viewBox="0 0 512 512"><path d="M64 32C28.7 32 0 60.7 0 96v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM344 152c-13.3 0-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24zm96-24c0 13.3-10.7 24-24 24s-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24zM64 288c-35.3 0-64 28.7-64 64v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V352c0-35.3-28.7-64-64-64H64zM344 408c-13.3 0-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24zm104-24c0 13.3-10.7 24-24 24s-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24z"></path></svg>
+              </a>
+
+            {/if}
+
+          </li>
           <li>
             {#if $page.route.id.includes('/d/courses')}
             <a class="flex items-center justify-center w-12 h-12 rounded-xl text-blue-500 hover:bg-gray-800 mb-2" sveltekit:prefetch href=/d/courses>
