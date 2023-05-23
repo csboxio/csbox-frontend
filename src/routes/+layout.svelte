fab811a43f12605bd85ee5a0b518497f156965f1
Ethan <ethanmccaskey2@gmail.com>
2023-05-23 13:43:21 -0700

    Notifications for creator of course works

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,11 +6,14 @@
 	import { get, readable, writable } from "svelte/store";
 	import { page } from "$app/stores";
 	import { notificationStore } from "../lib/stores/stores.js";
+	import Notification from '$lib/components/Notification.svelte';
+	import { each } from "svelte/internal";
 
 	/** @type {import('./$types').LayoutData} */
 	export let data;
 	export let notifications;
 	// Create a store and update it when necessary...
+	let store_notification;
 
 	onMount(() => {
 		const {
@@ -20,35 +23,59 @@
 			goto("/login");
 		});
 
+		const notificationsStored = notificationStore.subscribe(value => {
+			store_notification = value;
+			console.log(store_notification)
+		});
 
-		notifications =
-			supabaseClient
-				.from('notifications')
-				.select('*')
-				.then(({error, data}) => notificationStore.set(data))
+		// Get the courses that the user created.
+		supabaseClient
+			.from('courses')
+			.select('id')
+			.eq('created_by', $page.data.session?.user.id)
+			.then(({ data: courseIDs }) => {
+				const courseIDsList = courseIDs.map(course => course.id);
+				console.log(courseIDsList)
 
-			// Add the subscription to the supabase client
-			const channel = supabaseClient
-				.channel('schema-db-changes')
-				.on(
-					'postgres_changes',
-					{
-						event: '*',
-						schema: 'public',
-						table: 'enrollment'
-					},
-					(payload) => {
-						notificationStore.update(notifications => [...notifications, payload.new])
-						console.log(notifications, payload.new)
-					}
-				)
-				.subscribe()
+				// Get the notifications table
+				notifications =
+					supabaseClient
+						.from('notifications')
+						.select('*')
+						.then(({error, data}) => notificationStore.set(data))
 
+				// Add the subscription to the supabase client
+				if (courseIDsList) {
+					const channel = supabaseClient
+						.channel('schema-db-changes')
+						.on(
+							'postgres_changes',
+							{
+								event: '*',
+								schema: 'public',
+								table: 'enrollment',
+								// Filter only the courses that the user created.
+								filter: `course_id=in.(${courseIDsList})`
+							},
+							(payload) => {
+								// Update the notification store with the new information.
+								notificationStore.update(notifications => [...notifications, payload.new])
+								console.log(payload.new)
+								console.log(notificationStore)
+							}
+						)
+						.subscribe()
+					return () => {
+						subscription.unsubscribe();
+						channel.unsubscribe();
+					};
+				}
 
-		return () => {
-			subscription.unsubscribe();
-			channel.unsubscribe();
-		};
+				return () => {
+					subscription.unsubscribe();
+				};
+
+			});
 
 
 	});
@@ -62,4 +89,8 @@
 	<title>CSBOX</title>
 </svelte:head>
 <slot />
-
+{#if store_notification}
+	{#each store_notification as notification}
+	<Notification title="{notification.course_id}" content="{notification.enrolled}"/>
+	{/each}
+{/if}
