6e31c8c281c0227da4e34a6def871229bc47c925
Ethan <ethanmccaskey2@gmail.com>
2023-06-08 15:01:47 -0700

    Legal

---
+++ src/routes/legal/terms/+page.svelte
@@ -0,0 +1,229 @@
+<script>
+  let terms = `Terms and Conditions
+
+These terms and conditions ("Agreement") govern your use of the CSBOX LLC ("CSBOX," "Company," "we," or "us") educational technology platform, including any associated services and features (collectively referred to as the "Service"). By registering for an account and using the Service, you ("User" or "you") agree to be bound by this Agreement.
+
+Registration and User Accounts
+1.1 By registering for an account, you confirm that you are either a student or an authorized representative of a student, and you consent to abide by this Agreement.
+1.2 You must provide accurate and complete information during the registration process.
+1.3 If you are registering on behalf of a student, you confirm that you have the necessary authority to do so.
+
+Use of the Service
+2.1 The Service provided by CSBOX is intended for educational purposes only.
+2.2 You agree to use the Service in compliance with all applicable laws and regulations.
+2.3 CSBOX does not guarantee the availability, accuracy, or reliability of the content or features provided through the Service.
+2.4 You acknowledge that the Service may include third-party content or links to third-party websites, and CSBOX is not responsible for such content or websites.
+
+Privacy and Data Protection
+3.1 CSBOX respects your privacy and handles personal data in accordance with applicable privacy laws and regulations.
+3.2 However, due to the nature of technology systems, CSBOX cannot guarantee the absolute security of your data or the prevention of unauthorized access, data breaches, or leaks.
+3.3 By using the Service, you acknowledge and accept these inherent risks associated with data protection.
+
+Intellectual Property
+4.1 The Service and all associated intellectual property rights are owned by CSBOX or its licensors.
+4.2 You are granted a limited, non-exclusive, non-transferable license to use the Service solely for educational purposes.
+4.3 You may not copy, modify, distribute, sell, or lease any part of the Service without prior written permission from CSBOX.
+
+Limitation of Liability
+5.1 CSBOX shall not be liable for any direct, indirect, incidental, special, consequential, or exemplary damages arising out of or in connection with the use of the Service.
+5.2 CSBOX shall not be responsible for any disruptions, delays, or errors in the Service, including but not limited to technical malfunctions, server downtime, or network failures.
+5.3 You agree to indemnify, defend, and hold harmless CSBOX and its officers, directors, employees, and agents from any claims, damages, liabilities, and expenses arising out of your use of the Service.
+
+Governing Law and Jurisdiction
+6.1 This Agreement shall be governed by and construed in accordance with the laws of the state of Arizona, United States.
+6.2 Any disputes arising under or in connection with this Agreement shall be subject to the exclusive jurisdiction of the state and federal courts located in Arizona.
+
+Amendments
+7.1 CSBOX reserves the right to modify or amend this Agreement at any time. Updated versions will be posted on our website.
+7.2 It is your responsibility to review this Agreement periodically. Continued use of the Service after any modifications or amendments constitutes your acceptance of the revised terms.
+
+Entire Agreement
+8.1 This Agreement constitutes the entire agreement between you and CSBOX regarding the use of the Service and supersedes any prior agreements or understandings, whether written or oral, relating to the subject matter herein.
+
+Severability
+9.1 If any provision of this Agreement is found to be invalid, illegal, or unenforceable, the remaining provisions shall continue in full force and effect.
+9.2 The invalid, illegal, or unenforceable provision shall be replaced with a valid, legal, and enforceable provision that most closely reflects the original intent of the parties.
+
+Waiver
+10.1 The failure of CSBOX to enforce any provision of this Agreement shall not be deemed a waiver of such provision or the right to enforce it.
+10.2 Any waiver of any provision of this Agreement must be in writing and signed by an authorized representative of CSBOX.
+
+Compliance with FERPA and OCR
+11.1 CSBOX understands the importance of complying with the Family Educational Rights and Privacy Act (FERPA) and the regulations of the U.S. Department of Education's Office for Civil Rights (OCR).
+11.2 CSBOX takes appropriate measures to protect student information and ensures that personally identifiable information (PII) is handled in accordance with FERPA and OCR guidelines.
+
+Liability of Vendor Information Sharing
+12.1 CSBOX may collaborate with trusted vendors or third parties to provide certain features or services within the Service.
+12.2 CSBOX shall make reasonable efforts to ensure that these vendors or third parties comply with applicable privacy and data protection laws.
+12.3 CSBOX shall not be held liable for any breaches or mishandling of data by these vendors or third parties. However, CSBOX will promptly address any known issues to mitigate any potential risks.
+
+Protection of CSBOX from Legal Claims
+13.1 CSBOX takes measures to protect itself from legal claims related to the actions or behavior of parents, teachers, and staff using the Service.
+13.2 CSBOX disclaims any responsibility for the actions, comments, or behavior of parents, teachers, and staff using the Service.
+13.3 It is the responsibility of parents, teachers, and staff to comply with applicable laws, regulations, and policies when using the Service.
+13.4 CSBOX shall not be held liable for any legal claims arising from the actions, comments, or behavior of parents, teachers, and staff.
+
+Intellectual Property
+14.1 The Service and all associated intellectual property rights are owned by CSBOX or its licensors.
+14.2 You are granted a limited, non-exclusive, non-transferable license to use the Service solely for educational purposes.
+14.3 You may not copy, modify, distribute, sell, or lease any part of the Service without prior written permission from CSBOX.
+
+Limitation of Liability
+15.1 CSBOX shall not be liable for any direct, indirect, incidental, special, consequential, or exemplary damages arising out of or in connection with the use of the Service.
+15.2 CSBOX shall not be responsible for any disruptions, delays, or errors in the Service, including but not limited to technical malfunctions, server downtime, or network failures.
+15.3 You agree to indemnify, defend, and hold harmless CSBOX and its officers, directors, employees, and agents from any claims, damages, liabilities, and expenses arising out of your use of the Service.
+
+Governing Law and Jurisdiction
+16.1 This Agreement shall be governed by and construed in accordance with the laws of the state of Arizona, United States.
+16.2 Any disputes arising under or in connection with this Agreement shall be subject to the exclusive jurisdiction of the state and federal courts located in Arizona.
+
+Amendments
+17.1 CSBOX reserves the right to modify or amend this Agreement at any time. Updated versions will be posted on our website.
+17.2 It is your responsibility to review this Agreement periodically. Continued use of the Service after any modifications or amendments constitutes your acceptance of the revised terms.
+
+Entire Agreement
+18.1 This Agreement constitutes the entire agreement between you and CSBOX regarding the use of the Service and supersedes any prior agreements or understandings, whether written or oral, relating to the subject matter herein.
+
+Severability
+19.1 If any provision of this Agreement is found to be invalid, illegal, or unenforceable, the remaining provisions shall continue in full force and effect.
+19.2 The invalid, illegal, or unenforceable provision shall be replaced with a valid, legal, and enforceable provision that most closely reflects the original intent of the parties.
+
+Waiver
+20.1 The failure of CSBOX to enforce any provision of this Agreement shall not be deemed a waiver of such provision or the right to enforce it.
+20.2 Any waiver of any provision of this Agreement must be in writing and signed by an authorized representative of CSBOX.
+
+Misuse of the Software
+21.1 You agree not to misuse the software provided by CSBOX, including but not limited to:
+a) Reverse engineering, decompiling, or disassembling the software.
+b) Modifying, copying, or distributing the software without prior written permission from CSBOX.
+c) Using the software for any illegal, unauthorized, or unethical purposes.
+21.2 CSBOX reserves the right to monitor the use of the software to ensure compliance with this Agreement.
+21.3 In the event of any misuse or violation of this Agreement, CSBOX may suspend or terminate your access to the Service and take legal action as necessary.
+
+Limitation of Liability and Indemnification
+22.1 CSBOX shall not be liable for any direct, indirect, incidental, special, consequential, or exemplary damages arising out of or in connection with the use of the Service or the software.
+22.2 You agree to indemnify, defend, and hold harmless CSBOX, its officers, directors, employees, and agents from any claims, damages, liabilities, and expenses, including reasonable attorney fees, arising out of your use of the Service, your violation of this Agreement, or your infringement of any rights of a third party.
+
+Governing Law and Jurisdiction
+23.1 This Agreement shall be governed by and construed in accordance with the laws of the state of Arizona, United States.
+23.2 Any disputes arising under or in connection with this Agreement shall be subject to the exclusive jurisdiction of the state and federal courts located in Arizona.
+
+Amendments
+24.1 CSBOX reserves the right to modify or amend this Agreement at any time. Updated versions will be posted on our website.
+24.2 It is your responsibility to review this Agreement periodically. Continued use of the Service after any modifications or amendments constitutes your acceptance of the revised terms.
+
+Entire Agreement
+25.1 This Agreement constitutes the entire agreement between you and CSBOX regarding the use of the Service and supersedes any prior agreements or understandings, whether written or oral, relating to the subject matter herein.
+
+Severability
+26.1 If any provision of this Agreement is found to be invalid, illegal, or unenforceable, the remaining provisions shall continue in full force and effect.
+26.2 The invalid, illegal, or unenforceable provision shall be replaced with a valid, legal, and enforceable provision that most closely reflects the original intent of the parties.
+
+Waiver
+27.1 The failure of CSBOX to enforce any provision of this Agreement shall not be deemed a waiver of such provision or the right to enforce it.
+27.2 Any waiver of any provision of this Agreement must be in writing and signed by an authorized representative of CSBOX.
+
+Contract Law
+28.1 This Agreement constitutes a legally binding contract between you and CSBOX.
+28.2 You acknowledge that you have read and understood this Agreement, and you agree to be bound by its terms and conditions.
+
+Payment
+29.1 If the use of the Service requires payment, you agree to pay all applicable fees and charges as outlined in the pricing and payment terms provided by CSBOX.
+29.2 You are responsible for providing accurate and updated payment information, and you authorize CSBOX to charge the applicable fees to your chosen payment method.
+29.3 CSBOX uses secure payment processing methods to protect your payment information. However, CSBOX shall not be held liable for any unauthorized access or breaches of your payment information.
+
+Governing Law and Jurisdiction
+30.1 This Agreement shall be governed by and construed in accordance with the laws of the state of Arizona, United States.
+30.2 Any disputes arising under or in connection with this Agreement shall be subject to the exclusive jurisdiction of the state and federal courts located in Arizona.
+
+Amendments
+31.1 CSBOX reserves the right to modify or amend this Agreement at any time. Updated versions will be posted on our website.
+31.2 It is your responsibility to review this Agreement periodically. Continued use of the Service after any modifications or amendments constitutes your acceptance of the revised terms.
+
+Entire Agreement
+32.1 This Agreement constitutes the entire agreement between you and CSBOX regarding the use of the Service and supersedes any prior agreements or understandings, whether written or oral, relating to the subject matter herein.
+
+Severability
+33.1 If any provision of this Agreement is found to be invalid, illegal, or unenforceable, the remaining provisions shall continue in full force and effect.
+33.2 The invalid, illegal, or unenforceable provision shall be replaced with a valid, legal, and enforceable provision that most closely reflects the original intent of the parties.
+
+Ownership of Content
+33.1 Any content, including text, images, audio, video, or other materials, that is written, uploaded, or contributed to CSBOX by Users, instructors, or any other party, shall become the property of CSBOX.
+33.2 CSBOX reserves the right to use, reproduce, modify, adapt, publish, distribute, and display such content for the purposes of providing and improving the Service, without any obligation to compensate or seek further permission from the content contributors.
+
+Copyright Protection
+34.1 CSBOX respects intellectual property rights and expects Users to do the same.
+34.2 Users are solely responsible for ensuring that any material they upload or share on CSBOX does not infringe upon the copyrights or intellectual property rights of others.
+34.3 CSBOX shall not be held liable for any copyright infringements committed by Users.
+
+Ownership of Course Material
+35.1 Any course material, including curriculum, assignments, quizzes, and educational resources, created or used within the CSBOX platform, shall be owned by CSBOX.
+35.2 Instructors and educational institutions retain the right to use the course material within the CSBOX platform during the period of their contractual agreement with CSBOX.
+
+Binding Agreement
+36.1 This Agreement is binding upon and inures to the benefit of the parties and their respective successors, assigns, and legal representatives.
+36.2 This Agreement may not be assigned or transferred by you without prior written consent from CSBOX.
+
+Term and Termination
+37.1 This Agreement shall remain in effect until terminated by either party.
+37.2 CSBOX reserves the right to suspend or terminate your access to the Service at any time for violation of this Agreement or any other legitimate reason.
+
+Severability
+38.1 If any provision of this Agreement is found to be invalid, illegal, or unenforceable, the remaining provisions shall continue in full force and effect.
+38.2 The invalid, illegal, or unenforceable provision shall be replaced with a valid, legal, and enforceable provision that most closely reflects the original intent of the parties.
+
+Governing Law and Jurisdiction
+39.1 This Agreement shall be governed by and construed in accordance with the laws of the state of Arizona, United States.
+39.2 Any disputes arising under or in connection with this Agreement shall be subject to the exclusive jurisdiction of the state and federal courts located in Arizona.
+
+Third-Party Providers and Cloud Services
+40.1 CSBOX may engage third-party service providers and utilize cloud services to support and enhance the functionality of the Service.
+40.2 CSBOX undertakes due diligence in selecting reputable third-party providers and cloud services that maintain appropriate data protection and security measures.
+40.3 However, CSBOX shall not be held liable for any breaches or mishandling of data by these third-party providers or cloud services. CSBOX will make reasonable efforts to address any known issues to mitigate potential risks.
+
+Data Security and Breach Notification
+41.1 CSBOX implements reasonable security measures to protect the data and information stored within its systems from unauthorized access, breaches, or misuse.
+41.2 In the event of a data breach that may affect high school student data, CSBOX will comply with applicable data breach notification laws, promptly notifying affected parties as required by law.
+
+State and Local Laws
+42.1 CSBOX recognizes the importance of complying with state and local laws regarding the protection of high school student data.
+42.2 CSBOX will make reasonable efforts to comply with any additional requirements or restrictions imposed by state and local laws where it operates or provides services.
+
+Limitation of Liability
+43.1 CSBOX shall not be held liable for any damages, losses, or claims arising from the unauthorized access, breach, or misuse of high school student data, including but not limited to incidents resulting from hacking, cyberattacks, or other unauthorized activities.
+
+Privacy and Consent
+44.1 CSBOX respects the privacy of high school students and obtains necessary consents and permissions as required by applicable laws.
+44.2 By using the Service, Users acknowledge and consent to the collection, use, and processing of their data in accordance with CSBOX's Privacy Policy.
+
+User Responsibilities
+45.1 Users of the Service, including teachers, students, parents, administrators, and any other individuals involved, are responsible for complying with applicable laws, regulations, and CSBOX's terms and conditions.
+45.2 Users shall not misuse or share high school student data in violation of privacy laws or the rights of others.
+
+
+`;
+
+</script>
+
+<main>
+  <h1>Terms and Conditions</h1>
+  <div class="terms-container">
+    <pre>{@html terms}</pre>
+  </div>
+</main>
+
+<style>
+    main {
+        padding: 20px;
+    }
+
+    h1 {
+        font-size: 24px;
+        margin-bottom: 20px;
+    }
+
+    .terms-container {
+        font-size: 16px;
+        line-height: 1.5;
+    }
+</style>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -2,13 +2,29 @@
 	import type { ActionData } from './$types';
 	import { applyAction, enhance, type SubmitFunction } from '$app/forms';
 	import { invalidate } from '$app/navigation';
+	import { Register, Section } from "flowbite-svelte-blocks";
+	import { Button, Checkbox, Input, Label } from "flowbite-svelte";
+	import { Turnstile } from "svelte-turnstile";
+	import { browser } from "$app/environment";
 
 	export let form: ActionData;
 	let loading = false;
+
+	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+
+	let scale = tweened(1, { duration: 800, easing: cubicOut });
+	let boxShadow = tweened('0 0 0 rgba(0, 0, 0, 0)', { duration: 800, easing: cubicOut });
+
+	function startAnimation() {
+		scale.set(0.95);
+		boxShadow.set('0 0 10px rgba(0, 0, 0, 0.3)');
+		scale.set(1);
+		boxShadow.set('0 0 0 rgba(0, 0, 0, 0)');
+	}
 </script>
 
-<body class="antialiased bg-body text-body font-body">
-	<div class="">
+		<!--
 		<section class="h-screen relative pt-16 pb-36 bg-gradient-black2 overflow-hidden">
 			<img
 				class="absolute top-0 transform left-1/2 -translate-x-1/2"
@@ -31,7 +47,7 @@
 								{#if form?.incorrect}<p class="error">Invalid credentials!</p>{/if}
 								<div class="flex flex-wrap max-w-md mx-auto -m-2 mb-5">
 									<div class="w-full p-2">
-										<!--Email-->
+
 										<input
 											class="w-full px-5 py-3.5 text-gray-500 placeholder-gray-500 bg-white outline-none focus:ring-4 focus:ring-indigo-500 border border-gray-200 rounded-lg"
 											type="email"
@@ -41,7 +57,7 @@
 											value={form?.values?.email ?? ''}
 										/>
 									</div>
-									<!--Password-->
+
 									<div class="w-full p-2">
 										<input
 											class="w-full px-5 py-3.5 text-gray-500 placeholder-gray-500 bg-white outline-none focus:ring-4 focus:ring-indigo-500 border border-gray-200 rounded-lg"
@@ -85,5 +101,106 @@
 				</div>
 			</div>
 		</section>
-	</div>
+	-->
+
+<style>
+	.jaw-dropping-button {
+		display: inline-block;
+		overflow: hidden;
+		border: none;
+		outline: none;
+		background: linear-gradient(135deg, #1a202c, #3c366b, #1a202c);
+		background-size: 400% 400%;
+		background-position: center;
+		color: white;
+		padding: 1rem 2rem;
+		font-size: 1.2rem;
+		border-radius: 0.25rem;
+		transition: transform 0.3s ease-out, box-shadow 0.3s ease-out;
+		cursor: pointer;
+	}
+
+	.jaw-dropping-button:hover {
+		transform: scale(1.05);
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+		background-position: bottom right;
+	}
+</style>
+
+<body class="antialiased bg-body text-body font-body">
+<div class="">
+	<section class="h-screen relative pt-16 pb-36 bg-gray-900 overflow-hidden">
+		<div class="relative z-10 container mx-auto px-4">
+				<div class="flex flex-wrap -m-6">
+					<div class="w-full p-6">
+						<div class="md:max-w-xl  mx-auto">
+
+			<Section name="register" >
+				<Register href="/">
+			<svelte:fragment slot="top">
+				CSBOX
+			</svelte:fragment>
+			<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+				<form class="flex flex-col space-y-6" action="/">
+					<h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Create an account</h3>
+					<Label class="space-y-2">
+						<span>Your email</span>
+						<Input type="email" name="email" placeholder="name@example.com" required />
+					</Label>
+					<Label class="space-y-2">
+						<span>Your password</span>
+						<Input type="password" name="password" placeholder="••••••••••••" required />
+					</Label>
+					<Label class="space-y-2">
+						<span>Confirm password</span>
+						<Input type="password" name="confirm-password" placeholder="••••••••••••" required />
+					</Label>
+					<div class="items-start">
+
+						<ul
+							class="w-48 bg-white rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-600 divide-y divide-gray-200 dark:divide-gray-600"
+						>
+							<li class="p-1"><Checkbox>
+								<div></div> <a class="p-0.5 font-medium text-primary-600 hover:underline dark:text-primary-500 px-1"
+																						href="/legal/terms"> Terms and Conditions</a>
+							</Checkbox></li>
+							<li class="p-1"><Checkbox>
+								<div></div> <a class="p-0.5 font-medium text-primary-600 hover:underline dark:text-primary-500 px-1"
+																						href="/legal/privacy"> Privacy Policy</a>
+							</Checkbox></li>
+							<li class="p-1"><Checkbox>
+								<div></div> <a class="p-0.5 font-medium text-primary-600 hover:underline dark:text-primary-500"
+																						href="/legal/aup"> Acceptable Use Policy</a>
+							</Checkbox></li>
+						</ul>
+					</div>
+					{#if browser}
+						<div class="flex justify-center items-center">
+							<div class="">
+						<Turnstile siteKey="0x4AAAAAAAFpCF8-h1TYQKHV"/>
+						</div>
+						</div>
+					{/if}
+					<button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
+					 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600
+					 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Register</button>
+
+					<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+						Already have an account? <a
+						href="/login"
+						class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+					>Login here</a
+					>
+					</div>
+				</form>
+			</div>
+		</Register>
+			</Section>
+
+		</div>
+					</div>
+				</div>
+		</div>
+	</section>
+</div>
 </body>
