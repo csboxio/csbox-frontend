f8930da64361b4907b1b2d80b4321bf72e0b5d6d
Ethan <ethanmccaskey2@gmail.com>
2023-06-09 13:32:23 -0700

    oops

--- src/routes/d/(d)/courses/(course)/[slug]/assignments/+layout.ts
+++ src/routes/d/(d)/courses/(course)/[slug]/assignments/+layout.ts
@@ -1,7 +1,6 @@
 import { browser } from "$app/environment";
 import { redirect } from "@sveltejs/kit";
 export const ssr = false
-export const prerender = true
 export const load = (async ({ fetch, data, request, url, parent, params }) => {
   const parentData = await parent();
   const session = parentData.session
