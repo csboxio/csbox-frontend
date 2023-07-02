cf9296ea048cc763fc96d3e02656970a78cca4f4
Ethan <ethanmccaskey2@gmail.com>
2023-07-01 23:38:08 -0700

    t'

--- src/routes/api/people/+server.ts
+++ src/routes/api/people/+server.ts
@@ -1,24 +1,35 @@
 import type { RequestHandler } from "@sveltejs/kit";
 import { error, json, redirect } from "@sveltejs/kit";
+import {createClient} from "@supabase/supabase-js";
+import {PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL} from "$env/static/public";
 
 // TODO UNKNOWN IF TRULY SECURE
 // https://github.com/supabase/auth-helpers/issues/408
 /** @type {import('./$types').RequestHandler} */
 // @ts-ignore
-export const GET: RequestHandler = async ({ request, url, locals: { supabase, getSession }, event }) => {
+export const GET: RequestHandler = async ({ request, url, locals: { getSession }, event }) => {
+  const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY,
+      {
+        db: { schema: 'private' },
+        auth: {
+          persistSession: false,
+        }
+      });
+
   const session = await getSession()
 
-  if (!session) {
-    throw redirect(303, '/');
+  if (session) {
+    const course = url.searchParams.get('course')
+    const {data, error } = await supabase.from('enrollment')
+        .select('user_id,  course_id, enrolled, enrollment_date, users ( first_name, last_name )')
+        .eq('course_id', course)
+
+    console.log(data, error )
+    //event.setHeaders({
+    //  'cache-control': 'public, max-age=60, s-maxage=60'
+    //})
+
+    return json(data)
   }
-  const course = url.searchParams.get('course')
-  const {data } = await supabase.from('enrollment')
-    .select('user_id,  course_id, enrolled, enrollment_date, users ( first_name, last_name )')
-    .eq('course_id', course)
 
-  //event.setHeaders({
-  //  'cache-control': 'public, max-age=60, s-maxage=60'
-  //})
-
-  return json(data)
 }
--- src/routes/api/people/code/+server.ts
+++ src/routes/api/people/code/+server.ts
@@ -12,8 +12,11 @@ export const GET: RequestHandler = async ({ request, url, locals: { supabase, ge
     throw redirect(303, '/');
   }
   const course = url.searchParams.get('course')
+  // created_by = user_id
   const { error, data: res, status } = await supabase.rpc('new_enroll_code',
     {course_id: course, created_by: session.user.id})
+
+  console.log(error, res, status)
 
 
   return json({res, error, status})
