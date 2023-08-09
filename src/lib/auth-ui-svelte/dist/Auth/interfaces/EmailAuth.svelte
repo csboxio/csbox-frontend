036d7a3e99da7304e4afb6ee9c59633b71b2b795
Ethan <ethanmccaskey2@gmail.com>
2023-08-09 16:51:34 -0700

    Email Redirect

--- src/lib/auth-ui-svelte/dist/Auth/interfaces/EmailAuth.svelte
+++ src/lib/auth-ui-svelte/dist/Auth/interfaces/EmailAuth.svelte
@@ -31,8 +31,7 @@ async function handleSubmit() {
                 email,
                 password,
 				// Modifications
-				options: { captchaToken },
-				redirectTo: 'https://csbox.io/d'
+				options: { captchaToken, redirectTo: 'https://csbox.io/d' },
             });
             if (signInError)
                 error = signInError.message;
@@ -49,8 +48,7 @@ async function handleSubmit() {
                 email,
                 password,
 				// Modifications
-				options: { captchaToken },
-				redirectTo: 'https://csbox.io/d/profile/setup'
+				options: { captchaToken, redirectTo: 'https://csbox.io/d/profile/setup' },
             });
 			console.log(error)
             if (signUpError)
