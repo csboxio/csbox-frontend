8a46b9b8c47c1c48adb0010f0b074e1fa255f2a8
Ethan <ethanmccaskey2@gmail.com>
2023-08-23 23:39:28 -0700

    Grades

--- src/routes/d/(d)/courses/(course)/[slug]/grades/+page.svelte
+++ src/routes/d/(d)/courses/(course)/[slug]/grades/+page.svelte
@@ -28,29 +28,30 @@
 </script>
 
 <div class="w-full">
-	<section class="p-1 mt-4">
-		<div class="container">
-				<div>
-					<div class="relative overflow-x-auto sm: w-full">
-
-						<table class="table-auto w-full border  text-white">
+	<section class="container mx-auto p-6 ">
+		<div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+			<div class="w-full overflow-x-auto">
+				<table class="w-full">
 							<thead>
-							<tr class="bg-gray-800">
-								<th class="border border-gray-400 px-4 py-2"></th>
+							<tr class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+							<th class="px-4 py-3 border"></th>
 								{#each grades.assignments as assignment (assignment.assignment_id)}
-									<th class="border border-gray-400 px-4 py-2">{assignment.title}</th>
+									<th class="px-4 py-3 border">{assignment.title}</th>
 								{/each}
 								{#each grades.quizzes as quiz (quiz.quiz_id)}
-									<th class="border border-gray-400 px-4 py-2">{quiz.title}</th>
+									<th class="px-4 py-3 border">{quiz.title}</th>
 								{/each}
 							</tr>
 							</thead>
-							<tbody>
+					       <tbody class="bg-white">
+							{#if grades.users}
 							{#each grades.users as user (user.user_id)}
-								<tr>
-									<td class="border border-gray-400 px-4 py-2 bg-gray-800">{user.first_name} {user.last_name}</td>
+								<tr class="text-gray-700">
+									<td class="px-4 py-3 border">
+										{user.first_name} {user.last_name}
+									</td>
 									{#each grades.assignments as assignment (assignment.assignment_id)}
-										<td class="border border-gray-400 px-4 py-2">
+										<td class="px-4 py-3 border">
 											{#if user.grades && user.grades.assignments && assignment.assignment_id in user.grades.assignments}
 												{user.grades.assignments[assignment.assignment_id]}
 												{:else}
@@ -60,7 +61,7 @@
 										</td>
 									{/each}
 									{#each grades.quizzes as quiz (quiz.quiz_id)}
-										<td class="border border-gray-400 px-4 py-2">
+										<td class="px-4 py-3 border">
 											{#if user.grades && user.grades.quizzes && quiz.quiz_id in user.grades.quizzes}
 												{user.grades.quizzes[quiz.quiz_id]}%
 											{:else}
@@ -70,9 +71,13 @@
 									{/each}
 								</tr>
 							{/each}
+								{:else}
+								<tr class=" px-4 py-2 border">
+									No grades yet.
+								</tr>
+							{/if}
 							</tbody>
 						</table>
-					</div>
 			</div>
 		</div>
 	</section>
