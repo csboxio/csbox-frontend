fb257e8b364206c9e77ca8a2ec25c1af010c50ad
Ethan <ethanmccaskey2@gmail.com>
2023-12-23 16:04:35 -0700

    Course selector

--- src/lib/blocks/CoursePicker.svelte
+++ src/lib/blocks/CoursePicker.svelte
@@ -13,17 +13,17 @@
     import {addNotification} from "../utilities/notifications.js";
 
 
-    export let courses
-    export let hoverID
+    export let courses;
     export let selected;
 
-    let open;
-
-    let code_invalid = false;
-    $: code_invalid;
-
+    function handleCourseSelection(id) {
+        if (selected === id) {
+            selected = null; // Toggle off selection if clicked again
+        } else {
+            selected = id; // Select the clicked course
+        }
+    }
 </script>
-
 
 <div class="container m-4 mx-8">
     <div class="flex flex-wrap -mx-12 -mb-2">
@@ -35,12 +35,11 @@
                         <div class="relative mb-8 mx-4 cursor-pointer">
                             <div class=" min-w-xs max-w-xs">
                                 <div class="relative group">
-                                    <a on:click={() => { selected = id}}>
+                                    <a on:click={() => { handleCourseSelection(id) }}>
                                         <!-- Color line at the top -->
-                                        <div class="absolute top-0 left-0 w-full h-2 bg-{course_color}-500 glow  z-10 group-hover:scale-105 group-hover:top-[-6px] rounded-t-[25px]  "></div>
-                                        <div class="absolute group-hover:scale-105 -inset-0.5 bg-gradient-to-r from-gray-400 to-gray-400 rounded-lg blur opacity-0 group-hover:opacity-30 transition duration-1500 group-hover:duration-200 "></div>
                                         <div class="">
-                                            <div class="relative p-6 bg-gray-700 rounded-xl group-hover:scale-105 transition|local duration-1500 ">
+                                            <!-- Rest of your course box content -->
+                                            <div class="relative p-6 bg-gray-700 rounded-xl group-hover:scale-105 transition|local duration-1500 {selected === id ? 'border-2 border-blue-400' : 'border-0'}">
                                                 <img src={course_image_url === null ? 'https://dummyimage.com/150x150/000/fff' : course_image_url + '?t=' + inserted_at} class="relative inline-flex items-center justify-center w-20 h-20 mb-6 rounded-lg drop-shadow-2xl  mr-24" alt='Course Image' />
                                                 <div class="text-xl text-white font-bold mb-3 " style="word-break: break-word;">
                                                     {course_title === '' ? 'Course Name' : course_title}
@@ -62,4 +61,3 @@
         {/key}
     </div>
 </div>
-
