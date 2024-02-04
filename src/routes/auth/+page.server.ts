5e4ec090e6d97283945c5ba5c5e23d90dae48f4e
ethan-mcc <ethanmccaskey2@gmail.com>
2024-02-04 12:07:57 -0700

    fixed auth redirect issue

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,5 +1,12 @@
+import {browser} from "$app/environment";
 
-export const load = async ({ locals: { supabase } }) => {
-    await supabase.auth.signOut()
+export const load = async ({ context, locals: { supabase } }) => {
+
+    if (browser) {
+        if (context.url.pathname !== '/auth') {
+            await supabase.auth.signOut()
+        }
+    }
+
 }
 
